f777e61613373059a6a382ea177a2c3c40305606
Simona Bevilacqua <simonabevilacqua51@gmail.com>
2024-06-14 12:37:19 +0200

    aggiunta commenti knn

--- model_development.py
+++ model_development.py
@@ -6,19 +6,50 @@ from sklearn.model_selection import train_test_split
 
 class KNNClassifier:
     def __init__(self, k=3):
+        """
+                Inizializzazione del classificatore k-NN con un determinato valore di k.
+
+                Parameters:
+                k (int): Numero di vicini più prossimi da considerare.
+                """
         self.k = k
         self.X_train = None
         self.y_train = None
 
     def fit(self, X_train, y_train):
+        """
+                Addestramento del classificatore memorizzando i dati di training.
+
+                Parameters:
+                X_train (numpy.ndarray): Matrice delle caratteristiche di training.
+                y_train (numpy.ndarray): Array delle etichette di training.
+                """
         self.X_train = X_train
         self.y_train = y_train
 
     def predict(self, X_test):
+        """
+                Predizione delle etichette per i dati di test.
+
+                Parameters:
+                X_test (numpy.ndarray): Matrice delle caratteristiche di test.
+
+                Returns:
+                numpy.ndarray: Array delle etichette predette per i dati di test.
+                """
         predictions = [self._predict(x) for x in X_test]
         return np.array(predictions)
 
     def _predict(self, x):
+        """
+                Predizione dell'etichetta per un singolo campione di test.
+
+                Parameters:
+                x (numpy.ndarray): Singolo campione di test.
+
+                Returns:
+                int: Etichetta predetta per il campione di test.
+                """
         distances = [np.sqrt(np.sum((x - x_train) ** 2)) for x_train in self.X_train]
         k_indices = np.argsort(distances)[:self.k]
         k_nearest_labels = [self.y_train[i] for i in k_indices]
