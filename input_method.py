761cea0ffd21bbf872d446e00b362f922e780dd7
Simona Bevilacqua <simonabevilacqua51@gmail.com>
2024-06-21 13:50:32 +0200

    aggiunta codici finali

---
+++ input_method.py
@@ -0,0 +1,187 @@
+import pandas as pd
+
+class Input:
+    """
+    Classe per gestire l'input dell'utente riguardo al dataset, ai parametri del modello e alle metriche di valutazione.
+    """
+
+    def __init__(self, path_dataset=None, k=None, evaluation=None, training=None, K=None, metrics=[], data=None, target_column=None):
+        """
+        Inizializza la classe Input con i parametri forniti.
+
+        Parameters:
+        path_dataset (str): Il percorso del file del dataset
+        k (int): Il numero di vicini da utilizzare per il classificatore
+        evaluation (int): Il metodo di valutazione scelto (1 per Holdout, 2 per KFoldCrossValidation)
+        training (int): La percentuale di dati da utilizzare nel set di training
+        K (int): Il numero di fold per la validazione incrociata KFold
+        metrics (list): La lista delle metriche da calcolare
+        data (DataFrame): Il DataFrame contenente i dati del dataset
+        target_column (str): Il nome della colonna target nel dataset
+        """
+        self.k = k
+        self.evaluation = evaluation
+        self.training = training
+        self.K = K
+        self.metrics = metrics
+        self.path_dataset = path_dataset
+        self.data = data
+        self.target_column = target_column
+
+    def get_path(self):
+        """
+        Chiede all'utente di inserire il percorso del dataset e tenta di caricarlo
+
+        Stampa un messaggio di successo o un messaggio di errore se il file non viene trovato
+        """
+        while True:
+            self.path_dataset = input("Inserisci il path assoluto del tuo dataset: ")
+            if self.path_dataset.startswith('"') and self.path_dataset.endswith('"'):
+                self.path_dataset = self.path_dataset[1:-1]  # Rimuove i doppi apici
+            try:
+                self.data = pd.read_csv(self.path_dataset)
+                print("Dataset caricato con successo.")
+                break
+            except FileNotFoundError:
+                print(f"Il file '{self.path_dataset}' non è stato trovato.")
+
+    def get_k(self):
+        """
+        Chiede all'utente di inserire il numero di vicini da utilizzare per il classificatore KNN
+
+        Stampa un messaggio di errore se l'input non è un numero intero valido
+        """
+        while True:
+            try:
+                self.k = int(input("Inserisci il numero di vicini da utilizzare per il classificatore: "))
+                break
+            except ValueError:
+                print("Errore: Inserisci un numero intero valido.")
+
+    def get_evaluation_method(self):
+        """
+        Chiede all'utente di scegliere il metodo di valutazione del modello (Holdout o KFoldCrossValidation)
+
+        Stampa il metodo di valutazione scelto
+        """
+        print("Scegli come valutare il modello:")
+        print("1. Holdout")
+        print("2. KFoldCrossValidation")
+        while True:
+            try:
+                choice = int(input("Inserisci il numero corrispondente alla tua scelta: "))
+                if choice == 1:
+                    self.evaluation = 1
+                    break
+                elif choice == 2:
+                    self.evaluation = 2
+                    self.get_K()
+                    break
+                else:
+                    print("Errore: Inserisci un numero valido.")
+            except ValueError:
+                print("Errore: Inserisci un numero intero valido.")
+
+    def get_training_percentage(self):
+        """
+        Chiede all'utente di inserire la percentuale di dati da utilizzare nel set di training
+
+        Stampa un messaggio di errore se l'input non è un numero intero valido o non è compreso tra 0 e 100
+        """
+        while True:
+            try:
+                self.training = int(input("Inserisci la percentuale di dati da utilizzare nel set di training (0-100): "))
+                if 0 < self.training < 100:
+                    break
+                else:
+                    print("Errore: Inserisci un numero intero valido compreso tra 0 e 100.")
+            except ValueError:
+                print("Errore: Inserisci un numero intero valido.")
+
+    def get_K(self):
+        """
+        Chiede all'utente di inserire il numero di fold per la validazione incrociata KFold
+
+        Stampa un messaggio di errore se l'input non è un numero intero valido o non è maggiore di 0
+        """
+        while True:
+            try:
+                self.K = int(input("Inserisci il numero di esperimenti K: "))
+                if self.K > 0:
+                    break
+                else:
+                    print("Errore: Inserisci un numero intero valido maggiore di 0.")
+            except ValueError:
+                print("Errore: Inserisci un numero intero valido.")
+
+    def choose_metrics(self):
+        """
+        Chiede all'utente di scegliere le metriche da calcolare per la valutazione del modello
+
+        Aggiunge le metriche scelte alla lista self.metrics
+        """
+        print("Scegli quali metriche devono essere validate:")
+        print("1. Accuracy Rate")
+        print("2. Error Rate")
+        print("3. Sensitivity")
+        print("4. Specificity")
+        print("5. Geometric Mean")
+        print("6. Tutte le metriche disponibili")
+        while True:
+            choices = input("Inserisci i numeri corrispondenti alle metriche separate da uno spazio: ")
+            choices = choices.split()
+            valid_choices = ["1", "2", "3", "4", "5", "6"]
+            if all(choice in valid_choices for choice in choices):
+                break
+            else:
+                print("Errore: Inserisci numeri validi.")
+
+        for choice in choices:
+            if choice == "1":
+                self.metrics.append(1)
+            elif choice == "2":
+                self.metrics.append(2)
+            elif choice == "3":
+                self.metrics.append(3)
+            elif choice == "4":
+                self.metrics.append(4)
+            elif choice == "5":
+                self.metrics.append(5)
+            elif choice == "6":
+                self.metrics = [1, 2, 3, 4, 5]
+                break
+
+    def get_target_column(self):
+        """
+        Chiede all'utente di inserire il nome della colonna target
+
+        Verifica se la colonna esiste nel dataset. Stampa un messaggio di errore se la colonna non esiste
+        """
+        while True:
+            target_column = input("Inserisci il nome della colonna target: ")
+            if target_column in self.data.columns:
+                self.target_column = target_column
+                break
+            else:
+                print(f"Errore: La colonna '{target_column}' non esiste nel dataset.")
+
+    def get_input(self):
+        """
+        Chiede all'utente di fornire tutti gli input necessari:
+        - Percorso del dataset
+        - Numero di vicini per KNN
+        - Percentuale di training set
+        - Metodo di valutazione
+        - Metriche da calcolare
+        - Nome della colonna target
+
+        Returns:
+        self: L'istanza della classe Input con i valori forniti dall'utente
+        """
+        self.get_path()
+        self.get_k()
+        self.get_training_percentage()
+        self.get_evaluation_method()
+        self.choose_metrics()
+        self.get_target_column()
+        return self
---
+++ metrics.py
@@ -0,0 +1,365 @@
+import os
+import numpy as np
+from openpyxl import load_workbook
+import openpyxl
+import pandas as pd
+import matplotlib.pyplot as plt
+
+class Metrics:
+    """
+    Modella le metriche di validazione del modello.
+    """
+
+    def __init__(self, true_positive, true_negative, false_positive, false_negative, metrics=[], filename=None):
+        """
+        Inizializza la classe Metrics con i valori della matrice di confusione e le metriche specificate
+
+        Parameters:
+        true_positive (int): Il numero di veri positivi
+        true_negative (int): Il numero di veri negativi
+        false_positive (int): Il numero di falsi positivi
+        false_negative (int): Il numero di falsi negativi
+        metrics (list): La lista delle metriche da calcolare
+        filename (str): Il nome del file Excel per salvare i risultati
+        """
+        self.true_positive = true_positive
+        self.true_negative = true_negative
+        self.false_positive = false_positive
+        self.false_negative = false_negative
+        self.metrics = metrics
+        self.filename = filename
+
+    def confusion_matrix(self):
+        """
+        Calcola la matrice di confusione
+
+        Returns:
+        list: La matrice di confusione
+        """
+        confusion_matrix = [self.true_negative, self.false_positive, self.false_negative, self.true_positive]
+        return confusion_matrix
+
+    def accuracy(self, confusion_matrix, K=1):
+        """
+        Calcola l'accuratezza
+
+        Parameters:
+        confusion_matrix (list): La matrice di confusione.
+        K (int): Il numero di esperimenti.
+
+        Returns:
+        float: L'accuratezza media.
+        list: I valori di accuratezza per ogni esperimento.
+        """
+        accuracy_scores = []
+        true_positive = confusion_matrix[0]
+        false_positive = confusion_matrix[1]
+        false_negative = confusion_matrix[2]
+        true_negative = confusion_matrix[3]
+
+        # Se la matrice di confusione è una lista di liste
+        if isinstance(confusion_matrix[0], list):
+            for i in range(K):
+                accuracy_scores.append((true_positive[i] + true_negative[i]) / (true_positive[i] + false_positive[i] + false_negative[i] + true_negative[i]))
+        # Se la matrice di confusione è una lista singola
+        else:
+            accuracy_scores.append((true_positive + true_negative) / (true_positive + false_positive + false_negative + true_negative))
+
+        # Calcola l'accuratezza media
+        accuracy = np.mean(accuracy_scores)
+        return accuracy, accuracy_scores
+
+    def error_rate(self, confusion_matrix, K=1):
+        """
+        Calcola il tasso di errore.
+
+        Parameters:
+        confusion_matrix (list): La matrice di confusione.
+        K (int): Il numero di esperimenti.
+
+        Returns:
+        float: Il tasso di errore medio.
+        list: I valori di tasso di errore per ogni esperimento.
+        """
+        error_rate_scores = []
+        true_positive = confusion_matrix[0]
+        false_positive = confusion_matrix[1]
+        false_negative = confusion_matrix[2]
+        true_negative = confusion_matrix[3]
+
+        # Se la matrice di confusione è una lista di liste
+        if isinstance(confusion_matrix[0], list):
+            for i in range(K):
+                error_rate_scores.append((false_positive[i] + false_negative[i]) / (true_positive[i] + false_positive[i] + false_negative[i] + true_negative[i]))
+        # Se la matrice di confusione è una lista singola
+        else:
+            error_rate_scores.append((false_positive + false_negative) / (true_positive + false_positive + false_negative + true_negative))
+
+        # Calcola il tasso di errore medio
+        error_rate = np.mean(error_rate_scores)
+        return error_rate, error_rate_scores
+
+    def sensitivity(self, confusion_matrix, K=1):
+        """
+        Calcola la sensibilità.
+
+        Parameters:
+        confusion_matrix (list): La matrice di confusione.
+        K (int): Il numero di esperimenti.
+
+        Returns:
+        float: La sensibilità media.
+        list: I valori di sensibilità per ogni esperimento.
+        """
+        sensitivity_scores = []
+        true_positive = confusion_matrix[0]
+        false_positive = confusion_matrix[1]
+        false_negative = confusion_matrix[2]
+        true_negative = confusion_matrix[3]
+
+        # Se la matrice di confusione è una lista di liste
+        if isinstance(confusion_matrix[0], list):
+            for i in range(K):
+                sensitivity_scores.append(true_positive[i] / (true_positive[i] + false_negative[i]))
+        # Se la matrice di confusione è una lista singola
+        else:
+            sensitivity_scores.append(true_positive / (true_positive + false_negative))
+
+        sensitivity = np.mean(sensitivity_scores)
+        return sensitivity, sensitivity_scores
+
+    def specificity(self, confusion_matrix, K=1):
+        """
+        Calcola la specificità.
+
+        Parameters:
+        confusion_matrix (list): La matrice di confusione.
+        K (int): Il numero di esperimenti.
+
+        Returns:
+        float: La specificità media.
+        list: I valori di specificità per ogni esperimento.
+        """
+        specificity_scores = []
+        true_positive = confusion_matrix[0]
+        false_positive = confusion_matrix[1]
+        false_negative = confusion_matrix[2]
+        true_negative = confusion_matrix[3]
+
+        # Se la matrice di confusione è una lista di liste
+        if isinstance(confusion_matrix[0], list):
+            for i in range(K):
+                specificity_scores.append(true_negative[i] / (true_negative[i] + false_positive[i]))
+        # Se la matrice di confusione è una lista singola
+        else:
+            specificity_scores.append(true_negative / (true_negative + false_positive))
+
+        # Calcola la specificità media
+        specificity = np.mean(specificity_scores)
+        return specificity, specificity_scores
+
+    def geometric_mean(self, confusion_matrix, K=1):
+        """
+        Calcola la media geometrica.
+
+        Parameters:
+        confusion_matrix (list): La matrice di confusione.
+        K (int): Il numero di esperimenti.
+
+        Returns:
+        float: La media geometrica.
+        list: I valori di media geometrica per ogni esperimento.
+        """
+        geometric_mean_scores = []
+        true_positive = confusion_matrix[0]
+        false_positive = confusion_matrix[1]
+        false_negative = confusion_matrix[2]
+        true_negative = confusion_matrix[3]
+
+        # Se la matrice di confusione è una lista di liste
+        if isinstance(confusion_matrix[0], list):
+            for i in range(K):
+                sensitivity = true_positive[i] / (true_positive[i] + false_negative[i])
+                specificity = true_negative[i] / (true_negative[i] + false_positive[i])
+                geometric_mean_scores.append(np.sqrt(sensitivity * specificity))
+        # Se la matrice di confusione è una lista singola
+        else:
+            sensitivity = true_positive / (true_positive + false_negative)
+            specificity = true_negative / (true_negative + false_positive)
+            geometric_mean_scores.append(np.sqrt(sensitivity * specificity))
+
+        # Calcola la media geometrica media
+        geometric_mean = np.mean(geometric_mean_scores)
+        return geometric_mean, geometric_mean_scores
+
+    def get_metrics(self, K=1):
+        """
+        Calcola tutte le metriche.
+
+        Parameters:
+        K (int): Il numero di esperimenti.
+
+        Returns:
+        dict: Un dizionario contenente le metriche calcolate.
+        """
+        metrics = {}
+        confusion_matrix = self.confusion_matrix()
+
+        # Calcola le metriche richieste
+        if 1 in self.metrics:
+            metrics['Accuracy'] = self.accuracy(confusion_matrix, K)
+        if 2 in self.metrics:
+            metrics['Error Rate'] = self.error_rate(confusion_matrix, K)
+        if 3 in self.metrics:
+            metrics['Sensitivity'] = self.sensitivity(confusion_matrix, K)
+        if 4 in self.metrics:
+            metrics['Specificity'] = self.specificity(confusion_matrix, K)
+        if 5 in self.metrics:
+            metrics['Geometric Mean'] = self.geometric_mean(confusion_matrix, K)
+        if 6 in self.metrics:
+            metrics = {
+                'Accuracy': self.accuracy(confusion_matrix, K),
+                'Error Rate': self.error_rate(confusion_matrix, K),
+                'Sensitivity': self.sensitivity(confusion_matrix, K),
+                'Specificity': self.specificity(confusion_matrix, K),
+                'Geometric Mean': self.geometric_mean(confusion_matrix, K)
+            }
+        return metrics
+
+    def save_metrics(self, metrics, filename=None):
+        """
+        Salva le metriche su file Excel.
+
+        Parameters:
+        metrics (dict): Le metriche da salvare.
+        filename (str): Il nome del file Excel.
+
+        Returns:
+        None
+        """
+        if filename is None:
+            filename = input("Inserisci il nome del file Excel (senza estensione .xlsx): ")
+            filename = filename + '.xlsx' if '.xlsx' not in filename else filename  # Aggiungi l'estensione .xlsx se non è presente
+
+        self.filename = filename
+
+        metric_values = []
+        max_length = 0  # Lunghezza massima della colonna dei valori
+        for metric, value in metrics.items():
+            # Verifica se il valore è una lista
+            if isinstance(value, list):
+                # Se è una lista, formatta la lista come stringa
+                value_str = ', '.join([str(v) for v in value])
+            else:
+                value_str = value[1]
+            metric_values.append((metric, value_str))
+            max_length = max(max_length, len(str(value_str)))  # Aggiorna la lunghezza massima
+
+        df = pd.DataFrame(metric_values, columns=['Metriche', 'Valori'])
+        df.to_excel(filename, sheet_name='Risultati Metriche', index=False)
+
+        # Imposta la larghezza delle colonne utilizzando openpyxl
+        workbook = load_workbook(filename)
+        sheet = workbook['Risultati Metriche']
+        sheet.column_dimensions['A'].width = 20  # Imposta la larghezza della colonna A
+        sheet.column_dimensions['B'].width = max_length + 10  # Imposta la larghezza della colonna B
+        workbook.save(filename)
+
+        print("Le metriche sono state salvate su file Excel.")
+
+    def save_plot_to_excel(self, temp_file, sheet_name):
+        """
+        Salva un plot in un foglio Excel esistente o nuovo.
+
+        Parameters:
+        temp_file (str): Il nome dell'immagine temporanea del plot.
+        sheet_name (str): Il nome del foglio in cui salvare il plot.
+
+        Returns:
+        None
+        """
+        try:
+            # Carica il foglio Excel
+            wb = load_workbook(self.filename)
+            try:
+                ws = wb[sheet_name]
+            except KeyError:
+                # Se il foglio non esiste, crea un nuovo foglio
+                ws = wb.create_sheet(title=sheet_name)
+
+            # Aggiungi il plot al foglio Excel
+            img = openpyxl.drawing.image.Image(temp_file)
+            img.width = img.width * 0.8  # Riduci la larghezza dell'immagine del 20%
+            img.height = img.height * 0.8  # Riduci l'altezza dell'immagine del 20%
+            ws.add_image(img, 'A1')
+
+            # Salva le modifiche
+            wb.save(self.filename)
+
+            # Elimina il file temporaneo
+            try:
+                os.remove(temp_file)
+            except OSError as e:
+                print(f"Errore durante l'eliminazione del file temporaneo del plot.")
+        except Exception as e:
+            print(f"Errore durante il salvataggio del plot su Excel.")
+
+    def metrics_plot(self, metrics):
+        """
+        Mostra i grafici delle metriche.
+
+        Parameters:
+        metrics (dict): Le metriche da plottare.
+
+        Returns:
+        None
+        """
+        # Estraiamo le etichette delle metriche
+        labels = list(metrics.keys())
+        # Estraiamo le liste dei valori delle metriche
+        metric_scores = [value[1] for value in metrics.values()]
+
+        # Plot del boxplot
+        plt.figure(figsize=(12, 6))
+        plt.boxplot(metric_scores, labels=labels)
+
+        # Asse x
+        plt.xlabel('Metriche')
+        # Asse y
+        plt.ylabel('Valori')
+        # Titolo del grafico
+        plt.title('Metriche di validazione')
+        # Mostra la griglia
+        plt.grid(True)
+
+        # Salva il plot in un file temporaneo
+        temp_file = 'temp_plot1.png'
+        plt.savefig(temp_file)
+        self.save_plot_to_excel(temp_file, 'Plot Andamento Metriche')
+
+        # Estrazione dei dati per il plot a linea
+        K_experiments = list(range(1, len(metric_scores[0]) + 1))
+
+        # Plot per ciascuna metrica
+        plt.figure(figsize=(12, 6))
+        for label, values in zip(labels, metric_scores):
+            plt.plot(K_experiments, values, marker='o', label=label)
+
+        # Etichette e titoli
+        plt.xlabel('Esperimento K')
+        plt.ylabel('Valore della metrica')
+        plt.title('Andamento delle metriche negli esperimenti K')
+
+        # Mostra la legenda
+        plt.legend()
+
+        # Mostra il grafico
+        plt.grid(True)
+
+        # Salva il plot in un file temporaneo
+        temp_file = 'temp_plot2.png'
+        plt.savefig(temp_file)
+        self.save_plot_to_excel(temp_file, 'Andamento Metriche in K')
+
+        # Mostra il plot
+        plt.show()
