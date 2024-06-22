0f431dddc04aa987bede4ceb1d251593480505b0
Simona Bevilacqua <simonabevilacqua51@gmail.com>
2024-06-22 12:35:39 +0200

    codici aggiornati

--- evaluate_holdout.py
+++ evaluate_holdout.py
@@ -4,20 +4,10 @@ from metrics import Metrics
 
 class Holdout:
     """
-    Modella la tecnica di holdout per la suddivisione di un dataset in training set e test set
+    Modella la tecnica di holdout per la suddivisione di un dataset in training set e test set.
     """
 
     def __init__(self, data, target, metrics, k, train_size=0.8):
-        """
-        Inizializza la classe Holdout con i dati, il target, le metriche, il numero di vicini per KNN e la dimensione del training set
-
-        Parameters:
-        data (DataFrame): Il DataFrame contenente i dati del dataset
-        target (Series): Il Series contenente le etichette target del dataset
-        metrics (str): Le metriche da utilizzare per la valutazione
-        k (int): Il numero di vicini da considerare per il KNN
-        train_size (float): La proporzione del dataset da utilizzare come training set
-        """
         self.data = data
         self.target = target
         self.metrics = metrics
@@ -25,12 +15,6 @@ class Holdout:
         self.train_size = train_size
 
     def split(self):
-        """
-        Divide il dataset in training set e test set secondo la proporzione specificata
-
-        Returns:
-        numpy.ndarray, numpy.ndarray, numpy.ndarray, numpy.ndarray: I training set, test set, training target e test target
-        """
         train_index = np.random.choice(self.data.index, size=int(self.train_size * len(self.data)), replace=False)
         test_index = self.data.index[~self.data.index.isin(train_index)]
 
@@ -47,37 +31,25 @@ class Holdout:
         return train, test, train_target, test_target
 
     def evaluate(self):
-        """
-        Valuta il modello KNN utilizzando la suddivisione holdout del dataset
-
-        Returns:
-        dict: Le metriche calcolate della valutazione del modello
-
-        Stampa le metriche calcolate e salva le metriche su un file
-        """
         train, test, train_target, test_target = self.split()
 
         knn = KNNClassifier(self.k)
         knn.fit(train, train_target)
         predictions = knn.predict(test)
 
-        true_positive = 0
-        true_negative = 0
-        false_positive = 0
-        false_negative = 0
+        unique_labels = np.unique(self.target)
+        if len(unique_labels) != 2:
+            raise ValueError("Il modello attualmente supporta solo classificazioni binarie.")
+        positive_label = unique_labels[1]
+        negative_label = unique_labels[0]
 
-        for i in range(len(predictions)):
-            if predictions[i] == 4 and test_target[i] == 4:
-                true_positive += 1
-            elif predictions[i] == 4 and test_target[i] == 2:
-                false_positive += 1
-            elif predictions[i] == 2 and test_target[i] == 2:
-                true_negative += 1
-            elif predictions[i] == 2 and test_target[i] == 4:
-                false_negative += 1
+        true_positive = np.sum((predictions == positive_label) & (test_target == positive_label))
+        true_negative = np.sum((predictions == negative_label) & (test_target == negative_label))
+        false_positive = np.sum((predictions == positive_label) & (test_target == negative_label))
+        false_negative = np.sum((predictions == negative_label) & (test_target == positive_label))
 
         metrics = Metrics(true_positive, true_negative, false_positive, false_negative, self.metrics)
         output_metrics = metrics.get_metrics()
         metrics.save_metrics(output_metrics)
 
-        return output_metrics  # Restituisce le metriche calcolate
+        return output_metrics  # Assicurati di restituire le metriche calcolate
--- kfold.py
+++ kfold.py
@@ -4,21 +4,27 @@ from metrics import Metrics
 
 class KFoldCrossValidation:
     """
-    Modella la tecnica di k-fold cross validation per la suddivisione di un dataset in training set e test set
+    Modella la tecnica di k-fold cross validation per la suddivisione di un dataset in training set e test set.
     """
 
     def __init__(self, data, target, metrics, k, K, fold=[]):
         """
-        Inizializza la classe KFoldCrossValidation con i dati, il target, le metriche, il numero di vicini per KNN,
-        il numero di fold e la lista dei fold
+        Costruttore
 
-        Parameters:
-        data (DataFrame): Il DataFrame contenente i dati del dataset
-        target (Series): Il Series contenente le etichette target del dataset
-        metrics (list): La lista di metriche da calcolare
-        k (int): Il numero di vicini da considerare per il KNN
-        K (int): Il numero di fold
-        fold (list): Una lista contenente i fold:
+        Parameters
+        ----------
+        data : pandas.DataFrame
+            il dataset da suddividere
+        target : pandas.Series
+            la serie di valori target
+        metrics : list
+            la lista di metriche da calcolare
+        k : int
+            il valore di k per il modello KNN
+        K : int
+            il numero di fold
+        fold : list
+            una lista contenente:
             - il training set
             - il test set
             - i valori target del training set
@@ -33,34 +39,29 @@ class KFoldCrossValidation:
 
     def split(self):
         """
-        Suddivide il dataset in K fold, ognuno contenente un training set e un test set
+        Suddivide il dataset in training set e test set.
 
-        Returns:
-        list: Una lista contenente i fold:
+        Returns
+        -------
+         fold : list
+            una lista contenente:
             - il training set
             - il test set
             - i valori target del training set
             - i valori target del test set
         """
-        # Permuta gli indici del dataset
         indices = np.random.permutation(self.data.index)
-
-        # Generazione dei fold
         fold_size = int(len(self.data) / self.K)
         for i in range(self.K):
-            # Seleziona gli indici per il test set e per il training set
             test_index = indices[i * fold_size:(i + 1) * fold_size]
             train_index = indices[~np.isin(indices, test_index)]
 
-            # Generazione dei train set e test set
             train = self.data.loc[train_index]
             test = self.data.loc[test_index]
 
-            # Generazione dei target
             train_target = self.target.loc[train_index]
             test_target = self.target.loc[test_index]
 
-            # Converte i DataFrame e le Series in array numpy
             train = train.to_numpy()
             test = test.to_numpy()
             train_target = train_target.to_numpy()
@@ -71,12 +72,11 @@ class KFoldCrossValidation:
 
     def evaluate(self):
         """
-        Valuta le performance del modello KNN con k-fold cross validation
+        Valuta le performance del modello KNN con k-fold cross validation.
 
-        Returns:
-        dict: Le metriche aggregate calcolate dalla valutazione del modello
-
-        Stampa e salva le metriche aggregate, e visualizza un grafico delle metriche
+        Returns
+        -------
+        None
         """
         true_positive_list = []
         true_negative_list = []
@@ -84,44 +84,29 @@ class KFoldCrossValidation:
         false_negative_list = []
         self.split()
 
-        for i in range(self.K):
-            # Ottiene i set di addestramento e test per il fold corrente
-            train, test, train_target, test_target = self.fold[i]
-
-            # Creazione di un oggetto KNNClassifier con i parametri specificati
+        for fold in self.fold:
+            train, test, train_target, test_target = fold
             knn = KNNClassifier(self.k)
-
-            # Addestramento del modello KNN sul set di addestramento corrente
             knn.fit(train, train_target)
-
-            # Predizione dei valori target sul set di test corrente
             predictions = knn.predict(test)
 
-            # Calcolo delle metriche: true positive, true negative, false positive, false negative
-            true_positive = 0
-            true_negative = 0
-            false_positive = 0
-            false_negative = 0
-            for i in range(len(predictions)):
-                if predictions[i] == 4 and test_target[i] == 4:
-                    true_positive += 1
-                elif predictions[i] == 4 and test_target[i] == 2:
-                    false_positive += 1
-                elif predictions[i] == 2 and test_target[i] == 2:
-                    true_negative += 1
-                elif predictions[i] == 2 and test_target[i] == 4:
-                    false_negative += 1
+            unique_labels = np.unique(self.target)
+            if len(unique_labels) != 2:
+                raise ValueError("Il modello attualmente supporta solo classificazioni binarie.")
+            positive_label = unique_labels[1]
+            negative_label = unique_labels[0]
 
-            # Aggiunge i risultati del fold alle rispettive liste
+            true_positive = np.sum((predictions == positive_label) & (test_target == positive_label))
+            true_negative = np.sum((predictions == negative_label) & (test_target == negative_label))
+            false_positive = np.sum((predictions == positive_label) & (test_target == negative_label))
+            false_negative = np.sum((predictions == negative_label) & (test_target == positive_label))
+
             true_positive_list.append(true_positive)
             true_negative_list.append(true_negative)
             false_positive_list.append(false_positive)
             false_negative_list.append(false_negative)
 
-        # Calcola e salva le metriche aggregate e visualizza un grafico
         metrics = Metrics(true_positive_list, true_negative_list, false_positive_list, false_negative_list, self.metrics)
         output_metrics = metrics.get_metrics(self.K)
         metrics.save_metrics(output_metrics)
         metrics.metrics_plot(output_metrics)
-
-        return output_metrics
